e2a0fef4e52caa0640fce814752275570f20c8c8
Dominic Bruton <d.bruton@live.co.uk>
2020-02-15 11:27:12 +0000

    Add initial map test

---
+++ src/components/__tests__/Map.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
+import Map from '../Map';
+
+/* 
+Unable to test more using the real components as really is loding from google api
+Need to mock the google maps
+*/
+test('renders the loading component', async () => {
+  const wrapper = render(<Map />);
+  expect(wrapper.getByText('Loading...')).toBeTruthy();
+});
