47e318c74d20199514b740d0409f9535adf1c765
Dominic Bruton <d.bruton@live.co.uk>
2020-02-15 12:13:54 +0000

    Add marker with infobox

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { useState } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 import env from '../libs/env';
 
 function MapContainer(props) {
+
+  const [activeMarker, setActiveMarker] = useState();
+  const [isInfoOpen, setIsInfoOpen] = useState(false);
+
+
+  const onMarkerClicked = (props, marker) => {
+    if (isInfoOpen) {
+      setActiveMarker(null);
+      return setIsInfoOpen(false)
+    }
+    setActiveMarker(marker);
+    setIsInfoOpen(true);
+  };
+
   return (
     <Map
       google={props.google}
       zoom={8}
       style={mapStyles}
       initialCenter={{ lat: 47.444, lng: -122.176 }}
-    />
+    >
+      <Marker title="test" position={{ lat: 48.00, lng: -122.00 }} onClick={onMarkerClicked} />
+      <InfoWindow
+        marker={activeMarker}
+        visible={isInfoOpen}>
+        <div>
+          TEST PLACE
+        </div>
+      </InfoWindow>
+    </Map>
   );
 }
 
